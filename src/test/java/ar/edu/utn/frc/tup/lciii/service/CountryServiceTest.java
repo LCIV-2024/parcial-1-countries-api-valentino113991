a74792805ad1dcc74bf59c796beac1f1cb0007fb
113991-Bertello <113991@tecnicatura.frc.utn.edu.ar>
2024-10-05 16:32:56 -0300

    Mas tests

--- src/test/java/ar/edu/utn/frc/tup/lciii/service/CountryServiceTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lciii/service/CountryServiceTest.java
@@ -80,24 +80,89 @@ class CountryServiceTest {
     @Test
     void getAllPaisesDto() {
 
+        List<Map<String, Object>> listasApiExterna = List.of(
+                Map.of("name", Map.of("common", "Argentina"), "population", 1, "area", 10000, "region", "Sudamerica", "cca3", "ARG"),
+                Map.of("name", Map.of("common", "Italia"), "population", 1, "area", 5000, "region", "Europa", "cca3", "ITA")
+        );
+
+        when(restTemplate.getForObject(anyString(), eq(List.class))).thenReturn(listasApiExterna);
+
         Country country1 = Country.builder()
                 .code("ARG")
                 .name("Argentina")
                 .build();
 
         Country country2 = Country.builder()
-                .code("URU")
-                .name("Uruguay")
+                .code("ITA")
+                .name("Italia")
                 .build();
 
         List<Country> countries = new ArrayList<>();
         countries.add(country1);
+        countries.add(country2);
 
+        List<CountryDTO> result = this.countryService.getAllPaisesDto("argentina", null);
+        assertEquals(1, result.size());
+        assertEquals("Argentina", result.get(0).getName());
+    }
 
-        when(this.countryService.getAllCountries()).thenReturn(countries);
+    @Test
+    void getAllPaisesDto2() {
+
+        List<Map<String, Object>> listasApiExterna = List.of(
+                Map.of("name", Map.of("common", "Argentina"), "population", 1, "area", 10000, "region", "Sudamerica", "cca3", "ARG"),
+                Map.of("name", Map.of("common", "Italia"), "population", 1, "area", 5000, "region", "Europa", "cca3", "ITA")
+        );
+
+        when(restTemplate.getForObject(anyString(), eq(List.class))).thenReturn(listasApiExterna);
+
+        Country country1 = Country.builder()
+                .code("ARG")
+                .name("Argentina")
+                .build();
+
+        Country country2 = Country.builder()
+                .code("ITA")
+                .name("Italia")
+                .build();
+
+        List<Country> countries = new ArrayList<>();
+        countries.add(country1);
+        countries.add(country2);
+
         List<CountryDTO> result = this.countryService.getAllPaisesDto(null, null);
         assertEquals(2, result.size());
     }
+
+    @Test
+    void getAllPaisesDto3() {
+
+        List<Map<String, Object>> listasApiExterna = List.of(
+                Map.of("name", Map.of("common", "Argentina"), "population", 1, "area", 10000, "region", "Sudamerica", "cca3", "ARG"),
+                Map.of("name", Map.of("common", "Italia"), "population", 1, "area", 5000, "region", "Europa", "cca3", "ITA")
+        );
+
+        when(restTemplate.getForObject(anyString(), eq(List.class))).thenReturn(listasApiExterna);
+
+        Country country1 = Country.builder()
+                .code("ARG")
+                .name("Argentina")
+                .build();
+
+        Country country2 = Country.builder()
+                .code("ITA")
+                .name("Italia")
+                .build();
+
+        List<Country> countries = new ArrayList<>();
+        countries.add(country1);
+        countries.add(country2);
+
+        List<CountryDTO> result = this.countryService.getAllPaisesDto(null, "ita");
+        assertEquals(1, result.size());
+        assertEquals("ITA", result.get(0).getCode());
+    }
+
 
     @Test
     void getPaisesByContinente() {
